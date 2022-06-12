8bb5761312f787c99163f6a4575ba3fe1a30ec58
wealth3373 <64339392+wealth3373@users.noreply.github.com>
2022-06-12 16:22:11 +0900

    No.74_text_fix
    内容確認へ進む→予約内容を確定する
    
    こちらを、後に作成しました。
    No.76_text_fixは284行目を修正しました。

--- src/screens/Service/Step4/index.js
+++ src/screens/Service/Step4/index.js
@@ -278,7 +278,7 @@ export default function ServiceStep4() {
           </View>
 
           <View style={{marginTop: 37, paddingHorizontal: 16}}>
-            <Button label="内容確認へ進む" onPress={handleSubmit} />
+            <Button label="予約内容を確定する" onPress={handleSubmit} />
           </View>
           <View style={{marginTop: 11, paddingHorizontal: 16}}>
             <Button variant="secondary" label="情報の入力に戻る" onPress={() => navigation.goBack()} />
