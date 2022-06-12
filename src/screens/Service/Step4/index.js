403e7b27fc54e4e4c0ca2ec855f5eb276f8ec479
wealth3373 <64339392+wealth3373@users.noreply.github.com>
2022-06-12 16:10:26 +0900

    No.76_text_fix
    予約内容を確定→情報の入力に戻る

--- src/screens/Service/Step4/index.js
+++ src/screens/Service/Step4/index.js
@@ -281,7 +281,7 @@ export default function ServiceStep4() {
             <Button label="内容確認へ進む" onPress={handleSubmit} />
           </View>
           <View style={{marginTop: 11, paddingHorizontal: 16}}>
-            <Button variant="secondary" label="予約内容を確定" onPress={() => navigation.goBack()} />
+            <Button variant="secondary" label="情報の入力に戻る" onPress={() => navigation.goBack()} />
           </View>
         </ScrollView>
       </View>
