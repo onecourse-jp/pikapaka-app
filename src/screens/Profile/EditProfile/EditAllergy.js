11f7be680a739a54f2e3520cd21f8db9d036171c
itsuki takemoto <63700481+itsuki97@users.noreply.github.com>
2022-06-13 22:33:38 +0900

    modify：アレルギー

--- src/screens/Profile/EditProfile/EditAllergy.js
+++ src/screens/Profile/EditProfile/EditAllergy.js
@@ -63,7 +63,7 @@ export default function EditAllergy({route}) {
         if (response.status == 200) {
           dispatch(updateUserProfile(data.data));
           global.hideLoadingView();
-          Alert.alert("", "個人情報の編集が完了しました。", [
+          Alert.alert("", "アレルギーを更新しました。", [
             {
               text: "はい",
               onPress: () => {
