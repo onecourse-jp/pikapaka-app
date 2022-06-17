d521fe24b0bb45d3c20104639f66e1368f068dfa
hieultd <letrunghieu08031998@gmail.com>
2022-06-17 17:16:16 +0700

    update fix feedback

--- src/screens/TCareClinic/CommercialLaw/index.js
+++ src/screens/TCareClinic/CommercialLaw/index.js
@@ -12,19 +12,21 @@ import FooterComponent from "@components/Layout/Footer";
 export default function () {
   const colors = useThemeColors();
   const fonts = useThemeFonts();
-  const content = [
+  const DATA = [
     {name: "社名", content: "株式会社ピカパカ", reqireForm: false},
     {name: "所在地", content: "〒105-0011\n東京都港区芝公園3-1-8 芝公園アネックス4F", reqireForm: false},
     {name: "設立", content: "2018年12月1日", reqireForm: false},
     {name: "資本金", content: "398,900,000円（資本準備金を含む）", reqireForm: false},
     {name: "代表取締役", content: "原　基記", reqireForm: false},
-    {name: "お問い合わせ TEL", content:"03-6820-0995", reqireForm: false},
+    {name: "お問い合わせ TEL", content: "03-6820-0995", reqireForm: false},
     {name: "FAX", content: "03-3431-6811", reqireForm: false},
-    {name: "インターネットでのお問い合わせ先", content: "", reqireForm: true, text: "お問い合わせフォーム >"},
+    {name: "インターネットでのお問い合わせ先", content: "support_healthcare@pikapaka.co.jp", reqireForm: false},
     {
-      name: "事業内容", 
-      content: "ヘルスケア事業\n・医療機関向けDX支援サービス\n・PCR検査センター企画\n・運営サービス\n福利厚生事業\n・法人出張サポートサービス", 
-      reqireForm: false},
+      name: "事業内容",
+      content:
+        "ヘルスケア事業\n・医療機関向けDX支援サービス\n・PCR検査センター企画\n・運営サービス\n福利厚生事業\n・法人出張サポートサービス",
+      reqireForm: false,
+    },
     {
       name: "主要取引銀行",
       content: "みずほ銀行",
@@ -52,15 +54,17 @@ export default function () {
     },
     {
       name: "返品特約",
-      content: "購入後のキャンセルは不可。返金なし。災害等の不可抗力や不測の事態が生じたことにより商品の発送が完了できなかった場合は免責とさせていただき、全額返金いたします。",
+      content:
+        "購入後のキャンセルは不可。返金なし。災害等の不可抗力や不測の事態が生じたことにより商品の発送が完了できなかった場合は免責とさせていただき、全額返金いたします。",
       reqireForm: false,
     },
     {
       name: "顧客情報",
-      content: "お客様からお預かりした顧客情報は、オンライン診療時、医薬品の発送時、弊社サービスの紹介案内の用途以外には一切使用いたしません。",
+      content:
+        "お客様からお預かりした顧客情報は、オンライン診療時、医薬品の発送時、弊社サービスの紹介案内の用途以外には一切使用いたしません。",
       reqireForm: false,
     },
-     {
+    {
       name: "特記事項",
       content: "お客様と弊社との間で訴訟が生じた場合、東京地方裁判所を第一審の専属的な管轄裁判所とします。",
       reqireForm: false,
@@ -80,13 +84,22 @@ export default function () {
                 {title}
               </Text>
             </View>
-            {content.map((item, index) => {
+            {DATA.map((item, index) => {
               return (
                 <View key={index} style={{marginBottom: 20}}>
                   <Text style={{fontWeight: "700", fontSize: 14, lineHeight: 21, textAlign: "justify", color: colors.textHiragino}}>
                     {item.name}
                   </Text>
-                  <Text style={{fontWeight: "400", fontSize: 14, lineHeight: 21, textAlign: "justify", color: colors.textHiragino}}>
+                  <Text
+                    style={{
+                      fontWeight: "400",
+                      fontSize: 14,
+                      lineHeight: 21,
+                      marginTop: 2,
+                      textAlign: "justify",
+                      color: colors.textHiragino,
+                    }}
+                  >
                     {item.content}
                   </Text>
                   {item.reqireForm ? (
